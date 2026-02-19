563c12e58d8e6ce8a72348a5bb473cd17e1207ab
Donny Kurniawan <4dnnyk@gmail.com>
2026-02-19 12:49:21 +0800

    Initial commit spring

---
+++ src/test/java/com/github/donnyk22/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.donnyk22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
