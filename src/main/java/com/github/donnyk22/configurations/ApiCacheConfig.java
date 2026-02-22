0ea1ac596ce3c21694e416269171c14518af5d20
Donny Kurniawan <4dnnyk@gmail.com>
2026-02-23 01:11:35 +0800

    fix deprecated code

--- src/main/java/com/github/donnyk22/configurations/ApiCacheConfig.java
+++ src/main/java/com/github/donnyk22/configurations/ApiCacheConfig.java
@@ -7,8 +7,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -19,13 +19,13 @@ public class ApiCacheConfig {
 
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
+        RedisSerializer<Object> serializer = RedisSerializer.json();
+
         return RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(EXPIRATION))
             .disableCachingNullValues()
             .serializeValuesWith(
-                RedisSerializationContext.SerializationPair.fromSerializer(
-                    new GenericJackson2JsonRedisSerializer()
-                )
+                RedisSerializationContext.SerializationPair.fromSerializer(serializer)
             );
     }
 }
