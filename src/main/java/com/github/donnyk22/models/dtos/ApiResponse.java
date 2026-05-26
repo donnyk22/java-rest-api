a35fb6fc17a33e3aef7914c967c152372ecfa20d
Donny Kurniawan <4dnnyk@gmail.com>
2026-05-26 15:21:08 +0700

    add timestamp to the api response

--- src/main/java/com/github/donnyk22/models/dtos/ApiResponse.java
+++ src/main/java/com/github/donnyk22/models/dtos/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.github.donnyk22.models.dtos;
 
+import java.time.Instant;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -11,10 +13,12 @@ public class ApiResponse<T> {
     private int status;
     private String message;
     private T data;
+    private Instant timestamp;
 
     public ApiResponse(int status, String message, T data) {
         this.status = status;
         this.message = message;
         this.data = data;
+        this.timestamp = Instant.now();
     }
 }
